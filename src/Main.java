1de49d8e20fcd7f71b82a7f28bfcebc031f090a6
prochnost04 <121277435+prochnost04@users.noreply.github.com>
2023-05-18 22:25:39 +0530

    Singleton and setup

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // IntelliJ IDEA suggests fixing it
         System.out.printf("Hello and welcome!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
