e7a40a3c39b863e2af5402c1696fbc596228b149
prochnost04 <121277435+prochnost04@users.noreply.github.com>
2023-05-27 15:29:51 +0530

    Builder Pattern

--- src/Builder/Client.java
+++ src/Builder/Client.java
@@ -3,12 +3,12 @@ package Builder;
 public class Client {
     public static void main(String[] args) {
         Student student = Student.getBuilder()
-                .setName("Renu")
-                .setAge(25)
-                .setPsp(90.00)
-                .setUniversityName("ABC")
-                .setGradYear(2015)
-                .build();
+                                 .setName("Renu")
+                                 .setAge(25)
+                                 .setPsp(90.00)
+                                 .setUniversityName("ABC")
+                                 .setGradYear(2015)
+                                 .build();
 
 //        student.setPsp(100);
 
